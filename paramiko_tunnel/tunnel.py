e2e85870ac2df844e704167c1ffb12956b3821ec
Adam Delman <flyn@flyn.cc>
2017-08-05 17:44:33 +0300

    first commit

---
+++ paramiko_tunnel/tunnel.py
@@ -0,0 +1,170 @@
+import logging
+import selectors
+import socketserver
+import threading
+
+import paramiko
+
+logger = logging.getLogger(
+    name=__name__,
+)
+
+
+class Tunnel(
+    socketserver.ThreadingTCPServer,
+):
+    daemon_threads = True
+    allow_reuse_address = True
+
+    def __init__(
+        self,
+        paramiko_session,
+        dest_host,
+        dest_port,
+        bind_address_and_port=(
+            '',
+            0,
+        ),
+    ):
+        self.paramiko_session = paramiko_session
+        self.dest_host = dest_host
+        self.dest_port = dest_port
+
+        super().__init__(
+            server_address=bind_address_and_port,
+            RequestHandlerClass=SSHForwardingHandler,
+            bind_and_activate=True,
+        )
+        self.bind_address, self.bind_port = self.server_address
+        server_thread = threading.Thread(
+            target=self.serve_forever,
+        )
+        server_thread.daemon = True
+        server_thread.start()
+
+    def __str__(
+        self,
+    ):
+        return 'Tunnel to \'{dest_host}:{dest_port}\', bound at \'{bind_address}:{bind_port}\'.'.format(
+            dest_host=self.dest_host,
+            dest_port=self.dest_port,
+            bind_address=self.bind_address,
+            bind_port=self.bind_port,
+        )
+
+    def server_close(
+        self,
+    ):
+        pass
+
+
+class SSHForwardingHandler(
+    socketserver.BaseRequestHandler,
+):
+    buffer_size = 1024
+
+    def __init__(
+        self,
+        request,
+        client_address,
+        server,
+    ):
+        self.selector = selectors.DefaultSelector()
+        self.ssh_channel = None
+        super().__init__(request, client_address, server)
+
+    def _read_from_client(
+        self,
+        socket_obj,
+        mask,
+    ):
+        self._transfer_data(
+            src_socket=socket_obj,
+            dest_socket=self.ssh_channel,
+        )
+
+    def _read_from_channel(
+        self,
+        socket_obj,
+        mask,
+    ):
+        self._transfer_data(
+            src_socket=socket_obj,
+            dest_socket=self.request,
+        )
+
+    def _transfer_data(
+        self,
+        src_socket,
+        dest_socket,
+    ):
+        src_socket.setblocking(
+            False,
+        )
+        data = src_socket.recv(
+            self.buffer_size,
+        )
+        if len(data):
+            dest_socket.send(
+                data,
+            )
+
+    def handle(
+        self,
+    ):
+        peer_name = self.request.getpeername()
+        try:
+            self.ssh_channel = self.server.paramiko_session.get_transport().open_channel(
+                kind='direct-tcpip',
+                dest_addr=(
+                    self.server.dest_host,
+                    self.server.dest_port,
+                ),
+                src_addr=peer_name,
+            )
+        except Exception as error:
+            logger.error(
+                msg='Incoming request to {host}:{port} failed.'.format(
+                    host=self.server.dest_host,
+                    port=self.server.dest_port,
+                ),
+            )
+
+            raise paramiko.SSHException(
+                error,
+            )
+        else:
+            self.selector.register(
+                fileobj=self.ssh_channel,
+                events=selectors.EVENT_READ,
+                data=self._read_from_channel,
+            )
+            self.selector.register(
+                fileobj=self.request,
+                events=selectors.EVENT_READ,
+                data=self._read_from_client,
+            )
+            if self.ssh_channel is None:
+                logger.warning(
+                    msg='Incoming request to {host}:{port} was rejected by the SSH server.'.format(
+                        host=self.server.dest_host,
+                        port=self.server.dest_port,
+                    ),
+                )
+
+                return None
+
+            while True:
+                events = self.selector.select()
+                for key, mask in events:
+                    callback = key.data
+                    callback(
+                        socket_obj=key.fileobj,
+                        mask=mask,
+                    )
+
+    def __del__(
+        self,
+    ):
+        self.ssh_channel.close()
+        self.request.close()
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='paramiko_tunnel',
+    version='0.1',
+    packages=[
+        'paramiko_tunnel',
+    ],
+    install_requires=[
+        'paramiko',
+    ],
+    url='',
+    license='GPLv3',
+    author='adam.delman',
+    author_email='flyn@flyn.cc',
+    description='Tunnel'
+)
