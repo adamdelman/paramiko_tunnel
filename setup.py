621dd6ff31dbe7c8423d6716fc2808a47cf8f8a2
Adam Delman <flyn@flyn.cc>
2017-08-07 20:48:08 +0300

    Packaging fixes.

--- setup.py
+++ setup.py
@@ -9,9 +9,16 @@ setuptools.setup(
     install_requires=[
         'paramiko',
     ],
-    url='',
+    download_url='https://github.com/adamdelman/paramiko_tunnel/archive/0.1.tar.gz',
+    url='https://github.com/adamdelman/paramiko_tunnel',
     license='GPLv3',
-    author='adam.delman',
+    author='Adam Delman',
     author_email='flyn@flyn.cc',
-    description='Tunnel'
+    description='A lightweight library for creating SSH tunnels on top of Paramiko.',
+    keywords=[
+        'paramiko',
+        'ssh',
+        'tunnel',
+    ],
+
 )
